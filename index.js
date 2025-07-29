0022224ccef46c12f7fba64899f7cd787e799d36
sanjai042004 <sanjaiv642@gmail.com>
2025-07-29 15:53:39 +0530

    backend update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express = require("express");
 const todoRoute = require("./src/routes/todo.route");
 const { default: mongoose } = require("mongoose");
@@ -7,21 +8,21 @@ const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
-// 🛡️ Middleware
-app.use(cors());
-app.use(express.json()); // ✅ Most important middleware
 
-// 🔌 MongoDB options
+app.use(cors());
+app.use(express.json());
+
+
 const mongoOptions = {
   maxPoolSize: 20,
 };
 let isConnected = false;
 
-// 🔁 MongoDB Connect
+
 const connectToMongoDB = async () => {
   if (!isConnected) {
     try {
-      const mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.6gwdcnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+      const mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.b9qeg0t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
       await mongoose.connect(mongoUri, mongoOptions);
       isConnected = true;
@@ -33,7 +34,6 @@ const connectToMongoDB = async () => {
   }
 };
 
-// 🔁 Middleware to ensure Mongo connection
 app.use(async (_, res, next) => {
   try {
     await connectToMongoDB();
